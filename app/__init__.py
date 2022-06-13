770d989b921659d97cf40623eff494f2f566f808
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-06-13 02:08:00 -0400

    Added pictures to Project Section

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 
 class Proj:
-    def __init__(self, name, descrip, git, demo) -> None:
+    def __init__(self, name, descrip, git, demo, img) -> None:
         self.name = name
         self.descrip = descrip
         self.git = git
         self.demo = demo
+        self.img = img
 
 
 class Polaroid:
@@ -39,13 +40,13 @@ def index():
 
     projs = [
         Proj("Google Maps", "A project that resemble features of Google Maps. This project was divided into 4 Milestones that allowed further development of the map. These 4 milestones included: Implementation of functions that assisted in further milestones, Implementing and bettering the GUI, Implementation of Path-Finding Algorithms and Travelling-Salesman Algorithms.",
-             "https://google.com/", "https://github.com/"),
+             "https://google.com/", "https://github.com/", "https://www.google.com/images/branding/product/2x/maps_96in128dp.png"),
         Proj("Designing a Processor", "Design of a processor written in verilog, which supports LEDR and the push buttons. Processor is able to support instructions such as add, substract, move, move-top and branch. Quartus Prime software was used to implement this project in verilog. Processor was tested through test benches and ModelSim Simulations to ensure correctness.",
-             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs"),
+             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs", "https://www.google.com/images/branding/product/2x/maps_96in128dp.png"),
         Proj("Assembly Language Programs", "Collection of projects that is written in ARM. These projects utilizes I/O devices such as the LEDR, 7-Segment Display, Push Buttons, Switches and the A9-Private Timer. These programs were tested on a DE1-Soc Board.",
-             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs"),
+             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs", "https://www.google.com/images/branding/product/2x/maps_96in128dp.png"),
         Proj("Video Games", "Collection of video games that are created in teams. Video games created include video games that connect to the VGA display. These games use animations and input/output devices. Also, includes video games that are played on desktop or mobile",
-             "https://github.com/", "https://github.com/")
+             "https://github.com/", "https://github.com/", "https://www.google.com/images/branding/product/2x/maps_96in128dp.png")
     ]
 
     exps = [
