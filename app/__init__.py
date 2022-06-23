8fd0ff788972ec74302740248bcb3e8e36f1a1ce
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-06-22 21:58:18 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import datetime
 
 load_dotenv()
 app = Flask(__name__)
-mydb.connect()
+
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), 
     user = os.getenv("MYSQL_USER"), 
     password = os.getenv("MYSQL_PASSWORD"), 
@@ -25,7 +25,7 @@ class TimelinePost(Model):
     class Meta: 
         database = mydb 
 
-
+mydb.connect()
 mydb.create_tables([TimelinePost])
 #project
 class Proj:
