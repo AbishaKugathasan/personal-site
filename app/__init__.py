9f09e393109d882cf513f2be2f726968204dc596
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-06-26 18:47:36 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -105,7 +105,7 @@ def get_time_line_post():
     }
 
 
-@app.route('/api/timeline_post/<'id'>', methods =['DELETE'])
+@app.route('/api/timeline_post/'id'', methods =['DELETE'])
 def delete_time_line_post(id): 
     id = request.form['id']
     for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
