8adddfbb71ce8b8c4809645f08832d78cde4bfbc
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-06-26 18:56:47 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -105,9 +105,9 @@ def get_time_line_post():
     }
 
 
-@app.route('/api/timeline_post/1', methods =['DELETE'])
+@app.route('/api/timeline_post/{id}', methods =['DELETE'])
 def delete_time_line_post(): 
-    id = request.form['1']
+    id = request.form['id']
     for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
         if model_to_dict(p)['id'] == id 
             p.delete_instance()
