53cb27b78c5719072dd9e7ad6df56b3c1297d09b
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-06-26 04:27:50 -0400

    Edited flask app

--- app/__init__.py
+++ app/__init__.py
@@ -107,4 +107,6 @@ TimelinePost.select().order_by(TimelinePost.created_at.desc())
 
 @app.route('/timeline')
 def timeline(): 
-    return render_template('timeline.html', title="Timeline")
+    return render_template('timeline.html', title="Timeline", url = os.getenv("URL"), posts=posts, data=data )
+
+   
