be7dc1aba6fb0a16c9143fa0bec0b439cc85c66f
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-06-26 09:44:37 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -107,6 +107,6 @@ TimelinePost.select().order_by(TimelinePost.created_at.desc())
 
 @app.route('/timeline')
 def timeline(): 
-    return render_template('timeline.html', title="Timeline", timeline_post=timeline_post)
+    return render_template('timeline.html', title="Timeline")
 
    
