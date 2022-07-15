1b2054a57fd3c6d48ea41079bc959d2ce8813709
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-07-15 19:34:11 -0400

    CSS

--- app/__init__.py
+++ app/__init__.py
@@ -82,6 +82,7 @@ def index():
         Exp("Experience 3", ["point 1", "point 2", "point 3"]),
         Exp("Experience 4", ["point 1", "point 2", "point 3"]), 
         Exp("Experience 5", ["point 1", "point 2", "point 3"]), 
+        Exp("Experience 6", ["point 1", "point 2", "point 3"])
     ]
 
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), projects=projs, polaroids=pols, experiences=exps)
