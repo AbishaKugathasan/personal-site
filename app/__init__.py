ce07b2e313bf75b90a1974d21904a5d3521e93f0
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-07-15 01:15:43 -0400

    Projects Section

--- app/__init__.py
+++ app/__init__.py
@@ -67,14 +67,14 @@ pols = [
 def index():
 
     projs = [
-        Proj("Google Maps", "A project that resemble features of Google Maps. This project was divided into 4 Milestones that allowed further development of the map. These 4 milestones included: Implementation of functions that assisted in further milestones, Implementing and bettering the GUI, Implementation of Path-Finding Algorithms and Travelling-Salesman Algorithms.",
-             "https://google.com/", "https://github.com/", "https://www.google.com/images/branding/product/2x/maps_96in128dp.png"),
-        Proj("Designing a Processor", "Design of a processor written in verilog, which supports LEDR and the push buttons. Processor is able to support instructions such as add, substract, move, move-top and branch. Quartus Prime software was used to implement this project in verilog. Processor was tested through test benches and ModelSim Simulations to ensure correctness.",
-             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs", "https://png.pngtree.com/png-vector/20190224/ourlarge/pngtree-vector-processor-icon-png-image_699737.jpg"),
-        Proj("Assembly Language Programs", "Collection of projects that is written in ARM. These projects utilizes I/O devices such as the LEDR, 7-Segment Display, Push Buttons, Switches and the A9-Private Timer. These programs were tested on a DE1-Soc Board.",
-             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs", "https://hwu71.github.io/photos/asm.png"),
+        Proj("Google Maps", "A project that resemble features of Google Maps",
+             "https://google.com/", "https://github.com/"),
+        Proj("Designing a Processor", "Design of a processor written in verilog, which supports LEDR and the push buttons. Processor is able to support instructions such as add, substract, move, move-top and branch.",
+             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs"),
+        Proj("Assembly Language Programs", "Collection of projects that is written in ARM. These projects utilizes I/O devices such as the LEDR, 7-Segment Display, Push Buttons, Switches and the A9-Private Timer.",
+             "https://github.com/", "https://github.com/AbishaKugathasan/ECE243-Labs"),
         Proj("Video Games", "Collection of video games that are created in teams. Video games created include video games that connect to the VGA display. These games use animations and input/output devices. Also, includes video games that are played on desktop or mobile",
-             "https://github.com/", "https://github.com/", "https://i.pinimg.com/originals/01/d4/68/01d468fb5a1e507ebf3a899e96c1d14f.png")
+             "https://github.com/", "https://github.com/")
     ]
 
     exps = [
