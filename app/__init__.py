19ebcc678f73609f659c6c865fe5d886bdcf261e
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-07-15 20:26:45 -0400

    CSS

--- app/__init__.py
+++ app/__init__.py
@@ -78,11 +78,11 @@ def index():
 
     exps = [
         Exp("University of Toronto Robotics Association 🤖",  ["point 1", "point 2"]),
-        Exp("Experience 2", ["point 1", "point 2", "point 3"]),
-        Exp("Experience 3", ["point 1", "point 2", "point 3"]),
-        Exp("Experience 4", ["point 1", "point 2", "point 3"]),
-        Exp("Experience 5", ["point 1", "point 2", "point 3"]),
-        Exp("Experience 6", ["point 1", "point 2", "point 3"])
+        Exp("Experience 2", ["point 1", "point 2"]),
+        Exp("Experience 3", ["point 1", "point 2"]),
+        Exp("Experience 4", ["point 1", "point 2"]),
+        Exp("Experience 5", ["point 1", "point 2"]),
+        Exp("Experience 6", ["point 1", "point 2"])
     ]
 
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), projects=projs, polaroids=pols, experiences=exps)
