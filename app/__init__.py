3154bd0e99c21a5061061d588f71dcb939dc30eb
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-07-15 20:06:24 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -77,12 +77,13 @@ def index():
     ]
 
     exps = [
-        Exp("University of Toronto Robotics Association 🤖", ["Designer","Present"]),
-        Exp("Meta x MLH Fellowship 💻", ["Fellow","Present"]),
-        Exp("University of Toronto", ["Web Developer", "Present"]), 
-        Exp("Experience 4", ["point 1", "point 2"])
-        Exp("Experience 5", ["point 1", "point 2"]), 
-        Exp("Experience 6", ["point 1", "point 2"])
+        Exp("My Resume"),
+        Exp("University of Toronto Robotics Association 🤖",  ["point 1", "point 2", "point 3"])
+        Exp("Experience 2", ["point 1", "point 2", "point 3"]),
+        Exp("Experience 3", ["point 1", "point 2", "point 3"]),
+        Exp("Experience 4", ["point 1", "point 2", "point 3"]),
+        Exp("Experience 5", ["point 1", "point 2", "point 3"]),
+        Exp("Experience 6", ["point 1", "point 2", "point 3"])
     ]
 
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), projects=projs, polaroids=pols, experiences=exps)
