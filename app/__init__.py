84cdbb8882f052f5bcc67182fad6173b7b11c422
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-07-15 21:41:24 -0400

    Hobbies Section

--- app/__init__.py
+++ app/__init__.py
@@ -55,10 +55,10 @@ class Exp:
 
 
 pols = [
-    Polaroid("Caption of polaroid 1", "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B1936%2C1296%5D&w=3872&h=1936&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2019%2F08%2F05144705%2FGettyImages-96709750-1.jpg"),
-    Polaroid("Caption of polaroid 2", ".\static\img\R.jpg"),
-    Polaroid("Caption of polaroid 3",
-             "https://www.success.com/wp-content/uploads/2016/09/therealreasontravelingmakesyouhappy.jpg")
+    Polaroid("Swimming", "https://i.pinimg.com/736x/54/2d/e8/542de8a80f8a4c58cf42e37ee88f8ee9.jpg"),
+    Polaroid("Hiking", ".\static\img\R.jpg"),
+    Polaroid("Tennis",
+             "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5b77a570056595.5b96c6000f7ed.jpg")
 ]
 
 
