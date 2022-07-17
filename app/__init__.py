eb7c453a121271105fd1b8e9c15b90d2ee97c41c
AbishaKugathasan <77181669+AbishaKugathasan@users.noreply.github.com>
2022-07-17 00:14:32 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -54,9 +54,9 @@ class Exp:
         self.descrip = descrip
 
 class Cont: 
-    def __init__(self, name, descrip) -> None:
+    def __init__(self, name) -> None:
         self.name = name
-        self.descrip = descrip
+       
 
 
 pols = [
@@ -81,7 +81,7 @@ def index():
     ]
 
     conts = [
-        Cont("Email", ""),
+        Cont("Email"),
         Cont("LinkedIn")
     ]
 
